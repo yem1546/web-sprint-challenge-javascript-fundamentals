5c3fc076c1a740fb95ae3b90ba2673d72387ce77
yem1546 <87500366+yem1546@users.noreply.github.com>
2022-08-16 15:08:22 -0400

    Completed request 1

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function myFunction() {
 //myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
-
+// Because the nestedFunction is created within myFunction it has access to the external scope
 
 
 
@@ -30,11 +30,14 @@ function myFunction() {
 💡 NOTE: you may use a for loop for this function if you wish 
 */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(number) {
+  let sum = 0
+  for(let i = 0; i <= number; ++i){
+    sum += i
   }
- 
+  return sum
+  }
+ console.log(summation(4))
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
@@ -60,9 +63,14 @@ const zooAnimals = [
   💡 NOTE: the array returned should be an array of strings, and each string should follow this pattern: "name: {name}, scientific: {scientific name}"
   */
 
-  function animalNames(/*Your Code Here*/){
-    /*Your Code Here*/
+  function animalNames(arr){
+    let displayNames = []
+    arr.forEach((val) => {
+      displayNames.unshift(`name: ${val.animal_name}, scientific: ${val.scientific_name}`)
+    })
+    return displayNames
   }
+  console.log(animalNames(zooAnimals))
   
 
   /* 🦁🦁🦁 Request 2: .map() 🦁🦁🦁
